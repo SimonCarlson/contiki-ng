f6302d7749e3ccf90072f0906f626fa1c8f75df5
simoncarlson <cimon.carlson@gmail.com>
2019-03-20 13:27:40 +0100

    Basic behaviour of client. Needs manifest parsing, DTLS, COSE

--- examples/suitup/update-client.c
+++ examples/suitup/update-client.c
@@ -43,7 +43,9 @@
 #include "contiki-net.h"
 #include "coap-engine.h"
 #include "coap-blocking-api.h"
+//#include "coap-callback-api.h"
 #include "dev/button-sensor.h"
+#include <unistd.h>
 
 /* Log configuration */
 #include "coap-log.h"
@@ -52,26 +54,59 @@
 
 /* FIXME: This server address is hard-coded for Cooja and link-local for unconnected border router. */
 #define SERVER_EP "coap://[fe80::201:1:1:1]"
+#define VENDOR_ID "7473889aee98fffdcd1876"
+#define CLASS_ID "28718ff5930282177ccc14aefbcd1276"
+#define VERSION "1.0"
+#define INTERVAL 4
+#define TIMEOUT 1
 
-#define TOGGLE_INTERVAL 10
+static char query_data[128]; /* allocate some data for queries and updates */
+//static coap_request_state_t rd_request_state;
+static char image_url[128];
 
-PROCESS(er_example_client, "Erbium Example Client");
-AUTOSTART_PROCESSES(&er_example_client);
-
-static struct etimer et;
+PROCESS(update_client, "Update client");
+AUTOSTART_PROCESSES(&update_client);
 
 
 /* This function is will be passed to COAP_BLOCKING_REQUEST() to handle responses. */
 void
 client_chunk_handler(coap_message_t *response)
 {
+  printf("REGISTER CALLBACK\n");
   const uint8_t *chunk;
 
   int len = coap_get_payload(response, &chunk);
 
-  printf("|%.*s", len, (char *)chunk);
+  printf("Response: %.*s", len, (char *)chunk);
 }
-PROCESS_THREAD(er_example_client, ev, data)
+
+
+void
+manifest_parser(coap_message_t *response)
+{
+  printf("MANIFEST CALLBACK\n");
+  const uint8_t *chunk;
+
+  int len = coap_get_payload(response, &chunk);
+  strncpy(image_url, "update/image", strlen("update/image"));
+
+  printf("Response: %.*s", len, (char *)chunk);
+}
+
+
+void
+update_handler(coap_message_t *response)
+{
+  printf("UPDATE CALLBACK\n");
+  const uint8_t *chunk;
+
+  int len = coap_get_payload(response, &chunk);
+
+  printf("Response: %.*s", len, (char *)chunk);
+}
+
+
+PROCESS_THREAD(update_client, ev, data)
 {
   coap_endpoint_t server_ep;
   PROCESS_BEGIN();
@@ -80,36 +115,43 @@ PROCESS_THREAD(er_example_client, ev, data)
 
   coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
   coap_endpoint_print(&server_ep);
-
   /* receives all CoAP messages */
   coap_engine_init();
 
-  etimer_set(&et, TOGGLE_INTERVAL * CLOCK_SECOND);
+    printf("Send packet to update/register\n");
+    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+    coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+    coap_set_header_uri_path(request, "update/register");
 
-  while(1) {
-    PROCESS_YIELD();
+    // const char msg[] = "Toggle!";
+    // coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
 
-    if(etimer_expired(&et)) {
-      printf("--Toggle timer--\n");
-      coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
-      /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
-      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
-      coap_set_header_uri_path(request, ".well-known/core");
+    // Copy POST data into buffer
+    snprintf(query_data, sizeof(query_data) - 1, "?vid=%s&cid=%s&v=%s", "VENDOR_ID", "CLASS_ID", "VERSION");
+    int bytes = coap_set_header_uri_query(request, query_data); 
+    printf("URI QUERY: %s, LENGTH: %d\n", request->uri_query, bytes);
+    LOG_INFO_COAP_EP(&server_ep);
+    LOG_INFO_("\n");
 
-      const char msg[] = "Toggle!";
+    printf("Sleeping\n");
+    //sleep(4);
 
-      coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
+    COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+    printf("Registration done\n");
 
-      LOG_INFO_COAP_EP(&server_ep);
-      LOG_INFO_("\n");
+    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+    coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+    coap_set_header_uri_path(request, "update/manifest");
+    COAP_BLOCKING_REQUEST(&server_ep, request, manifest_parser);
+    printf("Manifest done\n");
 
-      COAP_BLOCKING_REQUEST(&server_ep, request, client_chunk_handler);
+    coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+    coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+    coap_set_header_uri_path(request, image_url);
+    COAP_BLOCKING_REQUEST(&server_ep, request, update_handler);
+    printf("Image done\n");
 
-      printf("\n--Done--\n");
-
-      etimer_reset(&et);
-    }
-  }
+    printf("\n--Done--\n");
 
   PROCESS_END();
 }
