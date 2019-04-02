2fc75e5dcada417ca4a192f05e48a2a4ac189e52
simoncarlson <cimon.carlson@gmail.com>
2019-04-02 14:13:16 +0200

    Declare manifest parts, call the parser, check the manifest. Using pre-filled buffer

--- examples/suitup/update-client.c
+++ examples/suitup/update-client.c
@@ -46,6 +46,7 @@
 #include "coap-blocking-api.h"
 #include "coap-keystore-simple.h"
 #include "rpl.h"
+#include "parse-test.h"
 //#include "coap-callback-api.h"
 
 /* Log configuration */
@@ -63,13 +64,13 @@
 #define TIMEOUT 1
 
 // TODO: Optimize, size of IDs known
-static char query_data[128]; /* allocate some data for queries and updates */
+//static char query_data[128]; /* allocate some data for queries and updates */
 //static coap_request_state_t rd_request_state;
-// TODO: Must be some upper bound for URLs
-static char image_url[128];
 // TODO: Assumption, fix dynamically?
-static char manifest_buffer[451];
+//static char manifest_buffer[451];
+char *manifest_buffer = "{\"0\": 1, \"1\": 1554114615, \"2\": [{\"0\": 0, \"1\": \"4be0643f-1d98-573b-97cd-ca98a65347dd\"}, {\"0\": 1, \"1\": \"18ce9adf-9d2e-57a3-9374-076282f3d95b\"}], \"3\": [], \"4\": 0, \"5\": {\"0\": 1, \"1\": 184380, \"2\": 0, \"3\": [{\"0\": \"update/image\", \"1\": \"ac526296b4f53eed4ab337f158afc12755bd046d0982b4fa227ee09897bc32ef\"}]}, \"6\": [], \"7\": [], \"8\": []}";
 static int manifest_offset = 0;
+char digest[256];
 
 PROCESS(update_client, "Update client");
 AUTOSTART_PROCESSES(&update_client);
@@ -97,7 +98,7 @@ manifest_callback(coap_message_t *response)
   int copied_bytes = strlen((char *)chunk);
   strncpy(manifest_buffer + manifest_offset, (char *)chunk, copied_bytes);
   manifest_offset += copied_bytes;
-  printf("Response: %s length: %d\n", (char *)chunk, strlen((char *)chunk));
+  printf("Response: %s length: %ld\n", (char *)chunk, strlen((char *)chunk));
 }
 
 
@@ -112,10 +113,14 @@ image_callback(coap_message_t *response)
   printf("Response: %.*s\n", len, (char *)chunk);
 }
 
-void
-manifest_parser() {
-  strncpy(image_url, "update/image", strlen("update/image"));
+
+int manifest_checker(manifest_t *manifest) {
+  printf("VENDOR ID: %d\n", strcmp(manifest->preConditions->value, VENDOR_ID));
+  printf("CLASS ID: %d\n", strcmp(manifest->preConditions->next->value, CLASS_ID));
+  strcpy(digest, manifest->payloadInfo->URLDigest->digest);
+  return 1;
 }
+
 
 PROCESS_THREAD(update_client, ev, data)
 {
@@ -131,9 +136,9 @@ PROCESS_THREAD(update_client, ev, data)
   coap_keystore_simple_init();
 
   etimer_set(&et, CLOCK_SECOND * INTERVAL);
-  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    printf("Send packet to update/register\n");
+    /*printf("Send packet to update/register\n");
     coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
     printf("CLIENT CONNECTED? : %d\n", coap_endpoint_is_connected(&server_ep));
     coap_endpoint_connect(&server_ep);
@@ -156,17 +161,51 @@ PROCESS_THREAD(update_client, ev, data)
 
     coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
     coap_set_header_uri_path(request, "update/manifest");
-    COAP_BLOCKING_REQUEST(&server_ep, request, manifest_callback);
-    manifest_parser();
+    COAP_BLOCKING_REQUEST(&server_ep, request, manifest_callback);*/
+    manifest_t m;
+    condition_t pre;
+    pre.type = 1;
+    pre.value = "0";
+    pre.next = NULL;
+    m.preConditions = &pre;
+    printf("%d\n", m.preConditions->type);
+
+    manifest_t manifest;
+    condition_t preConditions;
+    condition_t nextPreCondition;
+    condition_t postConditions;
+    payloadInfo_t payloadInfo;
+    URLDigest_t URLDigest;
+    URLDigest_t precursorImage;
+    URLDigest_t dependencies;
+    option_t options;
+
+    preConditions.next = &nextPreCondition;
+    manifest.preConditions = &preConditions;
+    manifest.postConditions = &postConditions;
+    payloadInfo.URLDigest = &URLDigest;
+    manifest.payloadInfo = &payloadInfo;
+    manifest.precursorImage = &precursorImage;
+    manifest.dependencies = &dependencies;
+    manifest.options = &options;
+    printf("Starting parser\n");
+    manifest_parser(&manifest, manifest_buffer);
+    print_manifest(&manifest);
+    int accept = manifest_checker(&manifest);
+    accept = 0;
     printf("Manifest done\n");
 
-    coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
-    coap_set_header_uri_path(request, image_url);
-    COAP_BLOCKING_REQUEST(&server_ep, request, image_callback);
-    printf("Image done\n");
-    printf("MANIFEST: %s\n", manifest_buffer);
-    printf("MANIFEST LENGTH: %d\n", strlen(manifest_buffer));
-    printf("\n--Done--\n");
+    if(accept) {
+      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+      coap_set_header_uri_path(request, manifest.payloadInfo->URLDigest->URL);
+      COAP_BLOCKING_REQUEST(&server_ep, request, image_callback);
+      printf("Image done\n");
+      printf("MANIFEST: %s\n", manifest_buffer);
+      printf("MANIFEST LENGTH: %ld\n", strlen(manifest_buffer));
+      printf("\n--Done--\n");
+    } else {
+      printf("Mismatched manifest.\n");
+    }
 
   PROCESS_END();
 }
