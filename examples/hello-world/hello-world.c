c0aa5fbb8cd73c2b9184ab5e647119448ae182db
simoncarlson <cimon.carlson@gmail.com>
2019-04-30 14:22:57 +0200

    Simple coap client with shell to test connectivity

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -38,28 +38,47 @@
  */
 
 #include "contiki.h"
+#include "contiki-net.h"
+#include "coap-engine.h"
+#include "coap-blocking-api.h"
+#include "coap-keystore-simple.h"
+
+#define INTERVAL 3
+#define SERVER_EP "coap://[fd00::1]"
 
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
 AUTOSTART_PROCESSES(&hello_world_process);
 /*---------------------------------------------------------------------------*/
+
+void hello_callback(coap_message_t *response) {
+    printf("REGISTER CALLBACK\n");
+}
+
 PROCESS_THREAD(hello_world_process, ev, data)
 {
-  static struct etimer timer;
-
   PROCESS_BEGIN();
+  static struct etimer et;
+  static coap_endpoint_t server_ep;
+  static coap_message_t request[1];
 
-  /* Setup a periodic timer that expires after 10 seconds. */
-  etimer_set(&timer, CLOCK_SECOND * 10);
+  coap_engine_init();
+  coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep);
+  // Connect to server endpoint
+  coap_endpoint_connect(&server_ep);
 
-  while(1) {
-    printf("Hello, world\n");
-
-    /* Wait for the periodic timer to expire and then restart the timer. */
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-    etimer_reset(&timer);
+  while(!coap_endpoint_is_connected(&server_ep)) {
+      etimer_set(&et, CLOCK_SECOND * INTERVAL);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      printf("Checking connection again\n");
+      coap_endpoint_connect(&server_ep);
   }
+  printf("CLIENT CONNECTED? : %d\n", coap_endpoint_is_connected(&server_ep));
+
+  coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+  coap_set_header_uri_path(request, "test/hello");
+  COAP_BLOCKING_REQUEST(&server_ep, request, hello_callback);
 
   PROCESS_END();
 }
