03bdee814d03dbf91699631efc8b76c98858c496
simoncarlson <cimon.carlson@gmail.com>
2019-05-01 14:18:23 +0200

    Parse data and save into profile file

--- examples/suitup/resources/res-register.c
+++ examples/suitup/resources/res-register.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include "cfs/cfs.h"
 #include "coap-engine.h"
 
 #include "coap-log.h"
@@ -33,22 +34,43 @@ static void
 res_register_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
   PRINTF("REGISTER RESOURCE\n");
-  const char *vendor_id;
-  //const char *class_id = NULL;
-  //const char *version = NULL;
-  /* Some data that has the length up to REST_MAX_CHUNK_SIZE. For more, see the chunk resource. */
-  char const *const message = "Hello World! ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
-  int length = 12; /*           |<-------->| */
+  char vendor_id[37];
+  char class_id[37];
+  char version[4];
+  const char *chunk;
 
-  if(coap_get_query_variable(request, "vid", &vendor_id)) {
-    printf("Vendor id: %s\n", vendor_id);
-    //memcpy(buffer, vendor_id, length);
-    //printf("Vendor id: %s\n", vendor_id);
-  }
+  coap_get_query_variable(request, "vid", &chunk);
+  PRINTF("CHUNK: %s\n", chunk);
+  char *p1, *p2;
+  // Find end of first argument
+  p2 = strstr((char *)chunk, "&");
+  memcpy(vendor_id, chunk, p2 - chunk);
+  vendor_id[36] = '\0';
+  PRINTF("Vendor id: %s\n", vendor_id);
+
+  // Find start and end of second argument
+  p1 = strstr(chunk, "=");
+  p2 = strstr(p1, "&");
+  memcpy(class_id, p1 + 1, p2 - p1 - 1);
+  class_id[36] = '\0';
+  PRINTF("Class id: %s\n", class_id);
+
+  // Advance past the delimiter
+  p1++;
+  p1 = strstr(p1, "=");
+  p2 = strstr(p1, "&");
+  // Version is 3 long
+  memcpy(version, p1 + 1, 3);
+  version[3] = '\0';
+  PRINTF("Version: %s\n", version);
+
+  char profile_data[37 + 37 + 4 + 1];
+  snprintf(profile_data, sizeof(profile_data), "%s\n%s\n%s\n", vendor_id, class_id, version);
+  int fd = cfs_open("profile", CFS_WRITE);
+  cfs_write(fd, profile_data, sizeof(profile_data));
+  cfs_close(fd);
+
 
   coap_set_status_code(response, CREATED_2_01);
-  memcpy(buffer, message, length);
   coap_set_header_content_format(response, TEXT_PLAIN); /* text/plain is the default, hence this option could be omitted. */
-  coap_set_header_etag(response, (uint8_t *)&length, 1);
-  coap_set_payload(response, buffer, length);
 }
