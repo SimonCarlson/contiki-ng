a38bd51565dcfaa0780682cfdc6fa0a280c2fc71
simoncarlson <cimon.carlson@gmail.com>
2019-05-01 12:24:18 +0200

    Run client on port 0, router on port 1

--- examples/suitup/run-client.sh
+++ examples/suitup/run-client.sh
@@ -1,1 +1,1 @@
-make MAKE_WITH_DTLS=1 TARGET=zoul BOARD=firefly PORT=/dev/ttyUSB1 update-client.upload && make MAKE_WITH_DTLS=1 TARGET=zoul BOARD=firefly PORT=/dev/ttyUSB1 login
+make MAKE_WITH_DTLS=1 TARGET=zoul BOARD=firefly PORT=/dev/ttyUSB0 update-client.upload && make MAKE_WITH_DTLS=1 TARGET=zoul BOARD=firefly PORT=/dev/ttyUSB0 login
